aa810148ab367d82636ab4891a60de3366e10769
Robert Henderson <robert@nterpryz.com>
2017-11-28 14:43:48 +0000

    Added Passenger To Queue

--- bus_stop.rb
+++ bus_stop.rb
@@ -1,14 +1,23 @@
 class BusStop
 
-  attr_reader :name
-
+  attr_reader :name, :queue
 
 
   def initialize(name)
     @name = name
+    @queue = []
   end
 
-  
+  def num_of_passengers_in_queue()
+    return @queue.length
+  end
+
+  def arrive(passenger)
+    @queue.push(passenger)
+  end
+
+
+
 
 
 end
--- specs/bus_stop_specs.rb
+++ specs/bus_stop_specs.rb
@@ -1,16 +1,28 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bus_stop")
+require_relative("../person")
 
 class TestBusStop < MiniTest::Test
 
   def setup
     @bus_stop = BusStop.new("Usher Hall")
+    @person1 = Person.new("Robert", 32)
   end
 
 
   def test_bus_stop_name
-    assert_equal("Usher Hall", @bus_stop.name) 
+    assert_equal("Usher Hall", @bus_stop.name)
   end
+
+  def test_bus_stop_queue
+    assert_equal(0, @bus_stop.queue.length)
+  end
+
+  def test_arrive
+    @bus_stop.arrive(@person1)
+    assert_equal(1, @bus_stop.num_of_passengers_in_queue)
+  end
+
 
 end
