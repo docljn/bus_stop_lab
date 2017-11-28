46aaed3421f1fc78df0313181567d23f716d1e0a
Robert Henderson <robert@nterpryz.com>
2017-11-28 13:12:20 +0000

    Added method to bus class returning passengers

--- bus.rb
+++ bus.rb
@@ -1,11 +1,19 @@
 class Bus
 
-  attr_reader :name
+  attr_reader :bus_number, :passengers, :destination
 
-  def initialize(bus_number, destination)
+  def initialize(bus_number, passengers, destination)
     @bus_number = bus_number
+    @passengers = passengers
     @destination = destination
   end
 
+  def drive
+    return "Brum Brum"
+  end
+
+  def passengers_on_bus
+    return @passengers.length
+  end
 
 end
--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -5,8 +5,16 @@ require_relative("../bus")
 class TestBus < MiniTest::Test
 
   def setup
-    @busroute = Bus.new(22, "Oceans Terminal")
+    @bus = Bus.new(22, [], "Oceans Terminal")
 
+  end
+
+  def test_drive
+    assert_equal("Brum Brum", @bus.drive())
+  end
+
+  def test_passengers_on_bus
+    assert_equal(0, @bus.passengers_on_bus())
   end
 
 
